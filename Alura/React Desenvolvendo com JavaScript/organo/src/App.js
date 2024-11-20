de0d9922546aab5cf77fb34fec84ee4e636b799f
Antônio Marcos <anttonimarco@gmail.com>
2024-11-20 00:35:53 -0300

    React Desenvolvendo com JavaScript

---
+++ Alura/React Desenvolvendo com JavaScript/organo/src/App.js
@@ -0,0 +1,13 @@
+import Banner from './Componentes/Banner/Banner';
+import CampoTexto from './Componentes/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <CampoTexto/>
+    </div>
+  );
+}
+
+export default App;
