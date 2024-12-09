9fe061c58d13ab580e031b95e44cf8702de2166b
Antônio Marcos <anttonimarco@gmail.com>
2024-12-08 21:29:35 -0300

    Efeito Scroll

---
+++ Diversos/Efeito Scroll/script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.h1', { duration: 3000});
+sr.reveal('.h2', {
+    rotate: { x: 0, y: 80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.h3', {
+    rotate: { x: 100, y: 100, z: 0},
+    duration: 3000
+});
