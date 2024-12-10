31d6bcc3e86b98b3b7106f7c69b95b32db244096
Antônio Marcos <anttonimarco@gmail.com>
2024-12-10 04:48:02 -0300

    Profile

---
+++ Profile/assets/scripts/script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.a1', { duration: 4000});
+sr.reveal('.a2', { duration: 4000});
+sr.reveal('.a3', { duration: 4000});
+
+sr.reveal('.header', {
+    rotate: { x: 0, y: 90, z:0},
+    duration: 2500
+});
